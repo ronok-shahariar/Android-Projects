aa9d3bbc365645fb1072de3267a0edf6c91b4fb3
ronok-shahariar <346ronokarya@gmail.com>
2021-04-17 16:32:07 +0600

    First coding

---
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onBtnClick (View view){
+        TextView txtHello = findViewById(R.id.txtMessage);
+        EditText editTextName = findViewById(R.id.edtTxtName);
+        txtHello.setText("Hello " + editTextName.getText().toString());
+
+    }
+}
