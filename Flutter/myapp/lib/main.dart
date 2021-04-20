23877c8e42bd375de7b39c4a531b675e4a37d45a
ronok-shahariar <346ronokarya@gmail.com>
2021-04-20 08:16:30 +0600

    Scaffold & AppBar Widgets,Colours & Fonts,Stateless Widgets & Hot Reload,Images & Assets,Buttons & Icons,Containers & Padding,Rows

--- Flutter/myapp/lib/main.dart
+++ Flutter/myapp/lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Hello Ronok!"),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title:Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text('hello world'),
+            ElevatedButton(onPressed: () {}, child: Text('Click me')),
+            Container(
+              color: Colors.cyan,
+              padding: EdgeInsets.all(30),
+              child: Text('Inside'),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+          backgroundColor: Colors.red[600],
+        ),
+    );
+  }
+}
+
 
 
